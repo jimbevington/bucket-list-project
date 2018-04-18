1b7a64f19f5d3ba1a62e4daf64ac65818795da47
Jim Bevington <jim.bevington@hotmail.com>
2018-04-18 15:13:34 +0100

    added test app.js, added script to index.html

---
+++ client/src/app.js
@@ -0,0 +1,14 @@
+
+
+
+const app = function(){
+
+  // populate dropdown
+  // make Request model, version for Countries api
+  // build
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', app);

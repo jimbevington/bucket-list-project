784a6f30bc9e7ddbe7eaa83955aae622a93bd7ef
Jim Bevington <jim.bevington@hotmail.com>
2018-04-18 15:23:32 +0100

    added request get method

--- client/src/services/request.js
+++ client/src/services/request.js
@@ -5,6 +5,17 @@ const Request = function(url){
 Request.prototype.get = function (callback) {
   const request = new XMLHttpRequest();
   request.open("GET", this.url);
+
+  request.addEventListener('load', function(){
+    if(this.status !== 200){
+      return;
+    }
+
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  });
+
+  request.send();
 };
 
 module.exports = Request;

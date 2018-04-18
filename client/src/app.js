da100dbbaf688a80658819ce69e96e31e3da3897
Jim Bevington <jim.bevington@hotmail.com>
2018-04-18 15:20:57 +0100

    added getAllCountries method to app

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,15 @@
+const Request = require('./services/request.js');
 
+const allCountriesRequest = new Request("https://restcountries.eu/rest/v2/all")
 
+const getAllCountries = function(){
+  allCountriesRequest.get(getAllCountriesRequestComplete);
+}
 
 const app = function(){
 
   // populate dropdown
+  getAllCountries();
   // make Request model, version for Countries api
   // build
 
--- client/src/services/request.js
+++ client/src/services/request.js
@@ -2,4 +2,9 @@ const Request = function(url){
   this.url = url;
 }
 
+Request.prototype.get = function (callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", this.url);
+};
+
 module.exports = Request;

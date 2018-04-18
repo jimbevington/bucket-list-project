80e091d80bc6f2e96426fb3512804103259b7123
Jim Bevington <jim.bevington@hotmail.com>
2018-04-18 18:55:02 +0100

    added formatCountryData method and part of render

--- client/src/views/countryView.js
+++ client/src/views/countryView.js
@@ -4,7 +4,37 @@ const CountryView = function(){
 
 CountryView.prototype.addCountry = function (country) {
   this.countries.push(country);
-  render(country);
+  this.render(country);
+};
+
+CountryView.prototype.render = function (country) {
+  const bucketList = document.getElementById('bucket-list');
+
+  const li = this.formatCountryData(country);
+  debugger;
+};
+
+CountryView.prototype.formatCountryData = function (country) {
+
+    const li = document.createElement('li');
+    country.className = 'country-list-item';
+    // name
+    const name = document.createElement('p');
+    name.className = 'countryName';
+    name.innerText = country.name;
+    // capital
+    const capital = document.createElement('p');
+    capital.innerText = `Capital: ${country.capital}`;
+    // population
+    const pop = document.createElement('p');
+    pop.innerText = `Population: ${country.population}`;
+    // flag
+    const flag = document.createElement('img');
+    flag.src = country.flag;
+
+    [name, capital, pop, flag].forEach(element => li.appendChild(element));
+
+    return li;
 };
 
 module.exports = CountryView;

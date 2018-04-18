704752fb39462759aefa41d68f44fe28fc29e9a8
Jim Bevington <jim.bevington@hotmail.com>
2018-04-18 15:36:31 +0100

    added selectelement to index, removed Request.get method

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,11 @@ const Request = require('./services/request.js');
 const allCountriesRequest = new Request("https://restcountries.eu/rest/v2/all")
 
 const getAllCountries = function(){
-  allCountriesRequest.get(getAllCountriesRequestComplete);
+  allCountriesRequest.get(populateCountriesList);
+}
+
+const populateCountriesList = function(allCountries){
+  
 }
 
 const app = function(){
